ce000a74536286edec354f535ff0bb0426d471b9
Le Hoai Vu <lehoaivu@Vu.local>
2024-03-20 14:05:23 +0700

    update preview link for app

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 const express = require('express');
 const path = require('path');
-const currentIP = require('./getIp')
 
 const app = express();
 const port = 3000;
+app.use(express.static('public'));
 
 // route to access HTML page
-app.get('/test', async(req, res) => {
+app.get('/*', async(req, res) => {    
+    const ipAddress = req.socket.remoteAddress;
+    // const ipAddresses = req.header('x-forwarded-for');
+    console.log('ipAddress :>> ', ipAddress);
+    /* Call API here */   
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  /* Call API here */   
-
-  while(currentIP) {
-      console.log('currentIP :>> ', currentIP);
-      res.redirect('https://youtube.com')
-      break
-  }
-
 });
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
