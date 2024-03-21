e2069e1a07e4ba4cadff14411d9385848bd47570
Le Hoai Vu <lehoaivu@Vu.local>
2024-03-21 07:42:23 +0700

    add new flaskapp

--- app.py
+++ app.py
@@ -6,41 +6,22 @@ https://github.com/petersimeth/basic-flask-template
 © MIT licensed, 2018-2023
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 DEVELOPMENT_ENV = True
 
 app = Flask(__name__)
 
 app_data = {
-    "name": "Peter's Starter Template for a Flask Web App",
-    "description": "A basic Flask app using bootstrap for layout",
-    "author": "Peter Simeth",
-    "html_title": "Peter's Starter Template for a Flask Web App",
-    "project_name": "Starter Template",
-    "keywords": "flask, webapp, template, basic",
+    "ip": ""
 }
 
 
 @app.route("/")
 def index():
+    ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    app_data['ip'] = ip
     return render_template("index.html", app_data=app_data)
-
-
-@app.route("/about")
-def about():
-    return render_template("about.html", app_data=app_data)
-
-
-@app.route("/service")
-def service():
-    return render_template("service.html", app_data=app_data)
-
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html", app_data=app_data)
-
 
 if __name__ == "__main__":
     app.run(debug=DEVELOPMENT_ENV)
